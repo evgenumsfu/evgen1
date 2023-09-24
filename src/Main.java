2dd0e51d8d0a5d069e76b7a4abdd88859eb0eab0
evgenumsfu <evgenumsfu@mail.ru>
2023-09-24 21:07:52 +0700

    1.Исправил римские цифры 2.Добавил метод calc

--- src/Main.java
+++ src/Main.java
@@ -1,83 +1,69 @@
 import java.io.IOException;
 import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.println("Введите пример");
-        int a;
-        int b;
-        boolean ar;
-
         String str1 = in.nextLine();
+        System.out.println(calc(str1));
+    }
+    public static String calc(String str1) {
+        int a=0;
+        int b=0;
+        boolean ar=true;
         String[] x = str1.split("[+*/?\\-]");
-
-        if (x.length > 2 ) try {
+        if (x.length >2) try {
             throw new IOException();
         } catch (IOException e) {
-            System.out.println("т.к. формат математической операции не удовлетворяет заданию " +
+            return ("т.к. формат математической операции не удовлетворяет заданию " +
                     "- два операнда и один оператор (+, -, /, *)");
-            return;
         }
-
-        if (x.length !=2 ) try {
+        if (x.length !=2) try {
             throw new IOException();
         } catch (IOException e) {
-            System.out.println("т.к. строка не является математической операцией");
-            return;
+            return ("т.к. строка не является математической операцией");
         }
-
-        if (RimArab.rimArabOff(x[0]) && !RimArab.rimArabOff(x[1])) try {
+        if (rimArabOff(x[0]) && !rimArabOff(x[1])) try {
             throw new IOException();
         } catch (IOException e) {
-            System.out.println("т.к. используются одновременно разные системы счисления");
-            return;
+            return ("т.к. используются одновременно разные системы счисления");
         }
-        if (!RimArab.rimArabOff(x[0]) && RimArab.rimArabOff(x[1])) try {
+        if (!rimArabOff(x[0]) && rimArabOff(x[1])) try {
             throw new IOException();
         } catch (IOException e) {
-            System.out.println("т.к. используются одновременно разные системы счисления");
-            return;
+            return ("т.к. используются одновременно разные системы счисления");
         }
-
-        if (RimArab.rimArabOff(x[0]) && RimArab.rimArabOff(x[1])) {     // на выхоте арабские цифры
+        if (rimArabOff(x[0]) && rimArabOff(x[1])) {     // на выхоте арабские цифры
             a = Integer.parseInt(x[0]);
             b = Integer.parseInt(x[1]);
-            ar = true;
-        } else if (!RimArab.rimArabOff(x[0]) && !RimArab.rimArabOff(x[1])) {     // на выхоте римские цифры
-            a = RimArab.convert(x[0]);
-            b = RimArab.convert(x[1]);
-            ar = false;
-        } else return;
-
+        } else if (!rimArabOff(x[0]) && !rimArabOff(x[1])) {     // на выхоте римские цифры
+            a = convert(x[0]);
+            b = convert(x[1]);
+            ar = false;}
         if (a < 0) try {
             throw new IOException();
-        } catch (
+        }
+        catch (
                 IOException e) {
-            System.out.println(" т.к. операнд не удовлетворяет заданию");
-            return;
+            return (" т.к. операнд не удовлетворяет заданию");
         }
         if (a > 10) try {
             throw new IOException();
         } catch (
                 IOException e) {
-            System.out.println("т.к. операнд не удовлетворяет заданию");
-            return;
+            return ("т.к. операнд не удовлетворяет заданию");
         }
         if (b < 0) try {
             throw new IOException();
         } catch (
                 IOException e) {
-            System.out.println("т.к. операнд не удовлетворяет заданию");
-            return;
+            return ("т.к. операнд не удовлетворяет заданию");
         }
         if (b > 10) try {
             throw new IOException();
         } catch (
                 IOException e) {
-            System.out.println("т.к. операнд не удовлетворяет заданию");
-            return;
-        }
+            return ("т.к. операнд не удовлетворяет заданию");}
         int rez = 0;
 
         if (str1.contains("+")) {
@@ -92,28 +78,23 @@ public class Main {
         if (str1.contains("/")) {
             rez = a / b;
         }
-        if ((rez <=0) && (!ar)) try {
+        if ((rez <= 0) && (!ar)) try {
             throw new IOException();
         } catch (
                 IOException e) {
-            System.out.println("т.к. в римской системе нет отрицательных чисел");
-            return;
+            return ("т.к. в римской системе нет отрицательных чисел");
         }
-
-        if (ar) System.out.println("Результат: " +rez);
-        if (!ar) System.out.println("Результат: " + RimArab.convert2 (rez));
+        if (!ar) return "Результат: " + convert2(rez);  // вывод римских чисел
+        return "Результат: " + rez;                     // вывод арабских чисел
     }
-}
-class RimArab {
     static String[] rim = new String[]{"0", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X","XI","XII",
-            "XIII", "XIV","XV","XVI", "XVII", "XVII", "XVIII","XIX","XX","XXI","XXII","XXIII","XXIV","XXV","XXVI",
+            "XIII", "XIV","XV","XVI", "XVII", "XVIII","XIX","XX","XXI","XXII","XXIII","XXIV","XXV","XXVI",
             "XXVII","XXVIII","XXIX","XXX","XXXI","XXXII","XXXIII","XXXIV","XXXV","XXXVI","XXXVII","XXXVIII","XXXIX",
             "XL","XLI", "XLII","XLIII","XLIV","XLV","XLVI","XLVII","XLVIII","XLIX","L","LI","LII","LIII","LIV","LV",
             "LVI","LVII","LVIII","LIX","LX","LXI","LXII","LXIII","LXIV","LXV","LXVI","LXVII","LXVIII","LXIX","LXX",
             "LXXI","LXXII","LXXIII","LXXIV","LXXV","LXXVI","LXXVII","LXXVIII","LXXIX","LXXX","LXXXI","LXXXII",
             "LXXXIII", "LXXXIV","LXXXV","LXXXVI","LXXXVII","LXXXVIII","LXXXIX","XC","XCI","XCII","XCIII","XCIV",
             "XCV","XCVI", "XCVII","XCVIII","XCIX","C"};
-
     public static boolean rimArabOff(String x) {
         for (String s : rim) {
             if (x.equals(s)) {
